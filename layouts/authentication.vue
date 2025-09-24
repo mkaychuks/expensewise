f78b68a682ecd2c96c9b0d1bc46eb08c5651e351
mkaychuks <sachufusi@gmail.com>
2025-09-24 15:01:23 +0100

    fix: routing

--- layouts/authentication.vue
+++ layouts/authentication.vue
@@ -2,9 +2,12 @@
 
 <template>
   <nav>
-    <div>
-      <NuxtLink to="/" class="font-bold">LOGOiPSUM</NuxtLink>
-    </div>
+    <NuxtLink to="/" class="font-bold"
+      ><div class="flex gap-1 items-center">
+        <img src="/logo.png" alt="logo" width="30" height="30" />
+        <h1>Expensewise</h1>
+      </div></NuxtLink
+    >
   </nav>
   <main>
     <slot />
